d00fedccf00181a2d51cc95e6f5fec3c2b002931
xiongzhuang <xiongzhuang@dcoin.com>
2018-11-19 17:48:33 +0800

    add summary

--- app/src/main/java/com/orangeaterz/designpatterns/策略模式/PriceCalculater.java
+++ app/src/main/java/com/orangeaterz/designpatterns/策略模式/PriceCalculater.java
@@ -1,15 +1,6 @@
 package com.orangeaterz.designpatterns.策略模式;
 
-/**
- * 策略模式
- * 优点：
- * 结构清晰明了、使用简单直观
- * 耦合度相对而言较低，扩展方便
- * 操作封装也更彻底，数据更为安全
- * <p>
- * 缺点：
- * 随着策略的增加，子类也会变得繁多
- */
+
 public class PriceCalculater {
     private CalculateStrategy mCalculateStrategy;
 
