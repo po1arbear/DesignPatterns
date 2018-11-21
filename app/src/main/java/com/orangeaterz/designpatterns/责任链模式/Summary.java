082bfead9e2cf0c0696a86c5d0ffeaa0226411ef
xiongzhuang <xiongzhuang@dcoin.com>
2018-11-21 09:56:35 +0800

    add summary

---
+++ app/src/main/java/com/orangeaterz/designpatterns/责任链模式/Summary.java
@@ -0,0 +1,14 @@
+package com.orangeaterz.designpatterns.责任链模式;
+
+/**
+ * 责任链模式
+ * 优点：
+ * 可以对请求者和处理着关系解耦，提高代码的灵活性
+ * <p>
+ * 缺点：
+ * 对链中请求处者的遍历，如果处理者太多会影响性能
+ * ，特别是在地柜调用中，要慎用
+ */
+
+public class Summary {
+}
