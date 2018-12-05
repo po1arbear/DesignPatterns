96a384f8756734440fbe54fea6841dd120111b00
xiongzhuang <xiongzhuang@dcoin.com>
2018-12-05 10:10:03 +0800

    add summary

---
+++ app/src/main/java/com/orangeaterz/designpatterns/迭代器模式/Summary.java
@@ -0,0 +1,13 @@
+package com.orangeaterz.designpatterns.迭代器模式;
+
+/**
+ * 迭代器模式
+ * 优点：
+ * 支持以不同的方式去遍历一个容器对象。也可以有多个遍历，
+ * 弱化了容器类与遍历算法之间的关系
+ * <p>
+ * 缺点：
+ * 类文件的增加
+ */
+public class Summary {
+}
