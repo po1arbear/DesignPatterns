441af67d96a91a04ac812b7b04b99d340c62f7c0
xiongzhuang <xiongzhuang@dcoin.com>
2018-12-29 21:29:08 +0800

    m

--- app/src/main/java/com/orangeaterz/designpatterns/代理模式/银行实战/BankActivity.java
+++ app/src/main/java/com/orangeaterz/designpatterns/代理模式/银行实战/BankActivity.java
@@ -24,7 +24,6 @@ public class BankActivity extends Activity implements View.OnClickListener {
         }
     };
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
